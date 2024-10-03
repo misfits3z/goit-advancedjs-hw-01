a7bbed6b8f522e430790fd9ffcd40acf9e7086bd
Didenko Victoria <155799784+misfits3z@users.noreply.github.com>
2024-10-03 12:26:41 +0800

    txt

---
+++ src/js/2-form.js
@@ -0,0 +1,47 @@
+let formData = { email: "", message: "" };
+
+      const form = document.querySelector('.feedback-form');
+      const emailInput = form.elements.email;
+      const messageInput = form.elements.message;
+
+      
+      const STORAGE_KEY = 'feedback-form-state';
+
+      const saveToLocalStorage = () => {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+      };
+
+
+      const loadFromLocalStorage = () => {
+        const savedData = localStorage.getItem(STORAGE_KEY);
+        if (savedData) {
+          formData = JSON.parse(savedData);
+          emailInput.value = formData.email || '';
+          messageInput.value = formData.message || '';
+        }
+      };
+
+      loadFromLocalStorage();
+
+      form.addEventListener('input', (event) => {
+        const { name, value } = event.target;
+        formData[name] = value.trim(); 
+        saveToLocalStorage(); 
+      });
+
+     
+      form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        
+        if (!formData.email || !formData.message) {
+          alert('Fill please all fields');
+          return;
+        }
+
+        console.log(formData);
+
+        localStorage.removeItem(STORAGE_KEY);
+        formData = { email: "", message: "" };
+        form.reset();
+      });
